fc9846911442a32725370f5ce1e150067740eacc
mona <hiba.al-aani@digitalcareerinstitute.org>
2023-08-23 10:30:18 +0200

    update

---
+++ 02_React_Component/lesson/vite-project/src/App.jsx
@@ -0,0 +1,58 @@
+import MainContainer from "./MainContainer";
+import Navbar from "./Navbar";
+
+function App() {
+  // write any javascript
+
+  const username = "Leon";
+  const tool = "Vite";
+
+  const userList = ["Sarah", "John", "Tom"];
+
+  const products = [
+    {name: "Product-1", id: 1},
+    {name: "Product-2", id: 2},
+    {name: "Product-3", id: 3},
+  ];
+
+  let isLoggedIn = true;
+
+  let themeMode = "Light";
+
+  return (
+    <>
+      {/* if we need to include js variables , we have to wrap it with {} */}
+      <h1>Hello world {tool}</h1>
+      <h3> Welcome Back {username}</h3>
+      <Navbar />
+
+      <MainContainer />
+
+      {userList.map((user, i) => (
+        <div key={i}>
+          <ul>
+            <li>{user}</li>
+          </ul>
+        </div>
+      ))}
+
+      {products.map((obj) => (
+        <div key={obj.id}>{obj.name}</div>
+      ))}
+
+      <footer>This is the footer</footer>
+
+      {/* conditional rendering a text or whatever , we can use ternary operators */}
+
+      {isLoggedIn ? <h2>Hello Leon</h2> : <h2>Hello there</h2>}
+
+      {themeMode === "Dark" ? (
+        <p>Now it's Dark mode</p>
+      ) : (
+        <p>Now it's light Mode</p>
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ 02_React_Component/lesson/vite-project/src/MainContainer.jsx
@@ -0,0 +1,18 @@
+function MainContainer() {
+  return (
+    <div>
+      <div>Hello Sam</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, ea.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, ea.
+      </p>{" "}
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam, ea.
+      </p>
+    </div>
+  );
+}
+
+export default MainContainer;
