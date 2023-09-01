cc6e5a9be6c6440d2dcb1f47d0f6b9e14dee4b21
nrcool <nomanalam7214@gmail.com>
2023-09-01 08:45:17 +0200

    fix debouncing

--- 07_React_Movies_Project/lesson/Movie-App/src/components/Search.jsx
+++ 07_React_Movies_Project/lesson/Movie-App/src/components/Search.jsx
@@ -1,26 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-function Search({handleSearch}) {
+function Search({ handleSearch }) {
   const [searchInput, setSearchInput] = useState("cast");
   //   console.log(searchInput);
 
+  const timer = useRef();
   const handleSubmit = (e) => {
     e.preventDefault();
     // the function we created in App.jsx to fetch data
     handleSearch(searchInput);
   };
 
+  //on mount fetch and display movies data
+  useEffect(() => {
+    //fetch data
+    /*   handleSearch(searchInput); */
+    // cleanup to clean the settimeOut before start a new setTimeout
+    return () => {
+      clearTimeout(timer.current);
+    };
+  }, []);
+
   //Debouncing:is a technique used to control how often a function is called
 
   useEffect(() => {
-    const timer = setTimeout(() => {
+    console.log(timer.current);
+    //if already a fetch is in progress ,cancel previous request
+    if (timer.current) {
+      clearTimeout(timer.current);
+    }
+    //make a new request after 1s
+    timer.current = setTimeout(() => {
       handleSearch(searchInput);
-    }, 700);
-
-    // cleanup to clean the settimeOut before start a new setTimeout
-    return () => {
-      clearTimeout(timer);
-    };
+    }, 1000);
   }, [searchInput]);
 
   return (
