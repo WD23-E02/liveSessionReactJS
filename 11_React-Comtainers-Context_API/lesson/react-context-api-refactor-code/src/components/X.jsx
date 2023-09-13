656d61f210733ff9e4307f67d30de4cef6f974c6
mona <hiba.al-aani@digitalcareerinstitute.org>
2023-09-13 13:49:31 +0200

    update

--- 11_React-Comtainers-Context_API/lesson/react-context-api-refactor-code/src/components/X.jsx
+++ 11_React-Comtainers-Context_API/lesson/react-context-api-refactor-code/src/components/X.jsx
@@ -1,14 +1,14 @@
-import React, { useContext } from 'react'
-import { MyContext } from "../context/reactContext"
+import React, {useContext} from "react";
+import {MyContext} from "../context/reactContext";
 /* import Button from './Button' */
 
 export default function X() {
-    const {x, setY} = useContext(MyContext)
+  const {x, setY} = useContext(MyContext);
   return (
-    <div style={{border:"2px solid white"}}>
-        <h1>X: {x} </h1>
-        <button onClick={()=>setY(y=>y+1)}>Increment Y</button>
+    <div style={{border: "2px solid white"}}>
+      <h1>X: {x} </h1>
+      <button onClick={() => setY((y) => y + 1)}>Increment Y</button>
       {/*   <Button color="primary">I am inside X component</Button> */}
     </div>
-  )
+  );
 }
